fdfa32c2602eab1e2e5878f338af542638e0819b
tanmay-ninawe <tanmay.ninawe72@gmail.com>
2026-04-20 20:08:16 +0530

    created Maven project and adjusted the structure

--- selenium-practice/docs/Session 3 - HTML Basics for Selenium.md
+++
@@ -1,4 +0,0 @@
-Session 3 - HTML Basics for Selenium 
-
-![](images/2026-03-20-09-22-13.png)
-![](images/2026-03-20-09-22-47.png)
--- selenium-practice/src/fistJavaClass.java
+++
@@ -1,8 +0,0 @@
-public class fistJavaClass {
-    public static void main(String [] args) {
-        
-        System.out.println("hey Buddy");
-
-    
-    }
-}
