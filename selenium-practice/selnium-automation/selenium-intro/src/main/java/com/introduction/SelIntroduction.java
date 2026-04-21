b1eec9c287359b993cd405c313f04ca2c2ca7d2c
Tanmay Ninawe <tanmay.ninawe72@gmail.com>
2026-04-21 09:25:20 +0530

    Enhance comments for browser invocation methods
    Added comments explaining two methods for invoking the browser.

--- selenium-practice/selnium-automation/selenium-intro/src/main/java/com/introduction/SelIntroduction.java
+++ selenium-practice/selnium-automation/selenium-intro/src/main/java/com/introduction/SelIntroduction.java
@@ -6,8 +6,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class SelIntroduction {
     public static void main(String[] args) {
        
-        //Invoking Browser
+    //Invoking Browser
+    //Method 1 - Using System.setProperty --> has to give the local browser driver path
+        //System.setProperty("webdriver.chrome.driver", "C:\\Switch\\Git-course\\chromedriver.exe");
+        //WebDriver driver = new ChromeDriver ();
+
+    //Method 2 - Using WebDriverManager --> no need to give the local browser driver path, it will automatically download the driver using WebDriverManager and seth the path.
         WebDriver driver = new ChromeDriver (); 
         driver.get("https://www.google.com");
+        
     }
 }
